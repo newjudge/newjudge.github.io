e0fc942866c2e5c82159770ca4faadcc8f223d7e
newjudge <135063038+newjudge@users.noreply.github.com>
2023-05-31 05:17:49 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-/* text.js */
-// span 요소 노드 가져오기
-const spanEl = document.querySelector("main h2 span");
-// 화면에 표시할 문장 배열
-const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
-// 배열의 인덱스 초깃값
-let index = 0;
-// 화면에 표시할 문장 배열에서 텍스트를 하나 가져온 뒤, 배열로 만들기
-let currentTxt = txtArr[index].split("");
+/* text_iife.js */
+// 텍스트 작성과 삭제 즉시 실행 함수
+(function(){
+  const spanEl = document.querySelector("main h2 span");
+  const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
+  let index = 0;
+  let currentTxt = txtArr[index].split("");
+  function writeTxt(){
+    spanEl.textContent  += currentTxt.shift(); 
+    if(currentTxt.length !== 0){ 
+      setTimeout(writeTxt, Math.floor(Math.random() * 100));
+    }else{
+      currentTxt = spanEl.textContent.split("");
+      setTimeout(deleteTxt, 3000);
+    }
+  }
+  function deleteTxt(){
+    currentTxt.pop();
+    spanEl.textContent = currentTxt.join("");
+    if(currentTxt.length !== 0){
+      setTimeout(deleteTxt, Math.floor(Math.random() * 100))
+    }else{
+      index = (index + 1) % txtArr.length;
+      currentTxt = txtArr[index].split("");
+      writeTxt();
+    }
+  }
+  writeTxt();
+})();
+/* end text_iife.js */
 
-function writeTxt(){ // ①
-  spanEl.textContent  += currentTxt.shift(); 
-  if(currentTxt.length !== 0){ // ②
-     setTimeout(writeTxt, Math.floor(Math.random() * 100));
-  }else{ // ③
-    currentTxt = spanEl.textContent.split("");
-    setTimeout(deleteTxt, 3000);
+/* scroll_request.js */
+/* 수직 스크롤이 발생하면 header 태그에 active 클래스 추가 및 삭제 */
+const headerEl = document.querySelector("header");
+window.addEventListener('scroll', function(){
+  requestAnimationFrame(scrollCheck);
+});
+function scrollCheck(){
+  let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+  if(browerScrollY > 0){
+    headerEl.classList.add("active");
+  }else{
+    headerEl.classList.remove("active");
   }
 }
+/* end scroll_request.js */
 
-function deleteTxt(){
-  currentTxt.pop(); // ①
-  spanEl.textContent = currentTxt.join(""); // ②
-  if(currentTxt.length !== 0){ // ③
-    setTimeout(deleteTxt, Math.floor(Math.random() * 100))
-  }else{ // ④
-    index = (index + 1) % txtArr.length;
-    currentTxt = txtArr[index].split("");
-    writeTxt();
-  }
+/* move.js */
+/* 애니메이션 스크롤 이동 */
+const animationMove = function(selector){
+  // ① selector 매개변로 이동할 대상 요소 노드 가져오기
+  const targetEl = document.querySelector(selector);
+  // ② 현재 브라우저의 스크롤 정보(y 값)
+  const browserScrollY = window.pageYOffset;
+  // ③ 이동할 대상의 위치(y 값)
+  const targetScorllY = targetEl.getBoundingClientRect().top + browserScrollY;
+  // ④ 스크롤 이동
+  window.scrollTo({ top: targetScorllY, behavior: 'smooth' });
+};
+// 스크롤 이벤트 연결하기
+const scollMoveEl = document.querySelectorAll("[data-animation-scroll='true']"); 
+for(let i = 0; i < scollMoveEl.length; i++){
+  scollMoveEl[i].addEventListener('click', function(e){
+    const target = this.dataset.target;
+    animationMove(target);
+  });
 }
-
-writeTxt();
-/* End text.js */
+/* End move.js */
